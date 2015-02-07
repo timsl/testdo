973235af2517bf1654765004f18c808eb663266a
timsl <tiol@kth.se>
2015-02-07 13:29:33 +0100

    Added redirect and some bugfixes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,18 +6,24 @@ angular.module('myApp.controllers', [])
   .controller('LandingPageController', [function() {
 
   }])
-  .controller('GuestlistController', ['$scope', '$firebase', function($scope, $firebase) {
+  .controller('GuestlistController', ['$scope', '$firebase', '$location', function($scope, $firebase, $location) {
   	// Skapa en referens till firebaseapplikationen
   	var guestsRef = new Firebase('https://guestdo-tim.firebaseio.com/');
 
   	// Passa referensen till firebase service
   	$scope.guests = $firebase(guestsRef);
 
-  	$scope.guest = {name: '', age: '', gender: '', city: '', country: ''};
+  	$scope.newGuest = {name: '', age: '', gender: '', city: '', country: '', checkedIn: 'false'};
 
   	$scope.saveGuest = function() {
   		// Add data till firebase
-  		$scope.guests.$add($scope.guest);
-  		$scope.guest = {name: '', age: '', gender: '', city: '', country: ''};
+  		$scope.guests.$add($scope.newGuest);
+  		$scope.newGuest = {name: '', age: '', gender: '', city: '', country: '', checkedIn: '', done: 'false'};
   	}
+
+  	$scope.login = function() {
+  		$location.path('/guestlist');
+  	}
+
+  	
   }]);
